28750e66007ec34e14883dae98045e6646bd2b97
mercury233 <me@mercury233.me>
2025-05-18 06:31:56 +0800

    more test

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,9 @@
+print("Host Arch: " .. os.hostarch())
+print("Target OS: " .. os.target())
+print("Target Arch: " .. (os.targetarch() or "nil"))
+print("_TARGET_ARCH: " .. (_TARGET_ARCH or "nil"))
+print("---")
+
 workspace "testWorkspace"
     configurations { "release" }
 
@@ -9,6 +15,12 @@ workspace "testWorkspace"
         system "macosx"
         architecture "x86_64"
 
+        print("test_x64:")
+        print("  Target OS: " .. os.target())
+        print("  Target arch: " .. (os.targetarch() or "nil"))
+        print("  _TARGET_ARCH: " .. (_TARGET_ARCH or "nil"))
+        print("---")
+
     project "test_arm64"
         kind "ConsoleApp"
         language "C++"
@@ -17,6 +29,12 @@ workspace "testWorkspace"
         system "macosx"
         architecture "ARM64"
 
+        print("test_arm64:")
+        print("  Target OS: " .. os.target())
+        print("  Target arch: " .. (os.targetarch() or "nil"))
+        print("  _TARGET_ARCH: " .. (_TARGET_ARCH or "nil"))
+        print("---")
+
     project "test_universal"
         kind "ConsoleApp"
         language "C++"
@@ -24,6 +42,12 @@ workspace "testWorkspace"
 
         system "macosx"
         architecture "universal"
+
+        print("test_universal:")
+        print("  Target OS: " .. os.target())
+        print("  Target arch: " .. (os.targetarch() or "nil"))
+        print("  _TARGET_ARCH: " .. (_TARGET_ARCH or "nil"))
+        print("---")
     
     project "test_universal_with_buildoptions"
         kind "ConsoleApp"
@@ -35,6 +59,12 @@ workspace "testWorkspace"
         buildoptions { "-arch arm64", "-arch x86_64" }
         linkoptions { "-arch arm64", "-arch x86_64" }
 
+        print("test_universal_with_buildoptions:")
+        print("  Target OS: " .. os.target())
+        print("  Target arch: " .. (os.targetarch() or "nil"))
+        print("  _TARGET_ARCH: " .. (_TARGET_ARCH or "nil"))
+        print("---")
+
     project "test_buildoptions"
         kind "ConsoleApp"
         language "C++"
@@ -43,3 +73,9 @@ workspace "testWorkspace"
         system "macosx"
         buildoptions { "-arch arm64", "-arch x86_64" }
         linkoptions { "-arch arm64", "-arch x86_64" }
+
+        print("test_buildoptions:")
+        print("  Target OS: " .. os.target())
+        print("  Target arch: " .. (os.targetarch() or "nil"))
+        print("  _TARGET_ARCH: " .. (_TARGET_ARCH or "nil"))
+        print("---")
